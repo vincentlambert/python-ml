213ffc33c8f25695d4702c5674ffbbbdf1975b50
Vincent Lambert <v.lambert@eurelis.com>
2018-03-04 20:23:18 +0100

    Split neural network Matplotlib heatmap

--- src/neural_network.py
+++ src/neural_network.py
@@ -105,7 +105,6 @@ class NeuralNetwork:
         # Adjust input->hidden weights
         self.weights_ih += weights_ih_deltas
 
-
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
@@ -114,7 +113,6 @@ class NeuralNetwork:
     def desigmoid(y):
         # return sigmoid(x) * (1 - sigmoid(x))
         return y * (1 - y)
-
 
 def testNN(nn):
     print(nn.predict([1, 0]))
@@ -146,38 +144,6 @@ def trainNN(nn, iterations):
         x = random.choice(data)
         nn.train(x['inputs'], x['target'])
 
-#
-# Draw heatmap
-#
-import matplotlib.pyplot as plt
-
-def draw_heatmap(neural_network):
-    step = 10
-    data = []
-    labels = []
-
-    for i in range(0, step + 1):
-        labels.append(i / step)
-
-    for x1 in range(0, step + 1):
-        data.append([])
-        for x2 in range(0, step + 1):
-            data[x1].append(neural_network.predict([x1 / step, x2 / step]).item())
-
-    # Mode 1
-    # fig, axis = plt.subplots()
-    # heatmap = axis.pcolor(data, cmap=plt.cm.Greys)
-    # axis.set_title('Network response')
-    # print(repr(fig))
-    # print(repr(axis))
-
-    # Mode 2
-    x, y = np.meshgrid(labels, labels)
-    intensity = np.array(data)
-    plt.pcolormesh(x, y, intensity, cmap=plt.cm.YlOrRd)
-    plt.colorbar()
-
-    plt.show()
 
 #
 # Main
@@ -200,6 +166,3 @@ if __name__ == '__main__':
     trainNN(nn, 100000)
     print('***** After training')
     testNN(nn)
-
-    # Plot heatmap
-    draw_heatmap(nn)
---
+++ src/neural_network_plt.py
@@ -0,0 +1,51 @@
+#
+# Draw neural network heatmap with Matplotlib
+#
+from neural_network import *
+import matplotlib.pyplot as plt
+
+def draw_heatmap(neural_network):
+    step = 10
+    data = []
+    labels = []
+
+    for i in range(0, step + 1):
+        labels.append(i / step)
+
+    for x1 in range(0, step + 1):
+        data.append([])
+        for x2 in range(0, step + 1):
+            data[x1].append(neural_network.predict([x1 / step, x2 / step]).item())
+
+    # Mode 1
+    # fig, axis = plt.subplots()
+    # heatmap = axis.pcolor(data, cmap=plt.cm.Greys)
+    # axis.set_title('Network response')
+    # print(repr(fig))
+    # print(repr(axis))
+
+    # Mode 2
+    x, y = np.meshgrid(labels, labels)
+    intensity = np.array(data)
+    plt.pcolormesh(x, y, intensity, cmap=plt.cm.YlOrRd)
+    plt.colorbar()
+    plt.show()
+
+#
+# Main
+#
+if __name__ == '__main__':
+    print("*****")
+    print("***** Main...")
+    print("*****")
+
+    # Neural network training for XOR
+    nn = NeuralNetwork(2, 3, 1)
+    # print('***** Before training')
+    # testNN(nn)
+    # trainNN(nn, 100000)
+    # print('***** After training')
+    # testNN(nn)
+
+    # Plot heatmap
+    draw_heatmap(nn)
