8072b0852505cca31794b83a7c7f2f47accc8c0a
Vincent Lambert <v.lambert@eurelis.com>
2018-03-24 17:02:14 +0100

    Genetic algorithm ready neural network for unsupervised learning

--- src/neural_network/neural_network.py
+++ src/neural_network/neural_network.py
@@ -1,24 +1,108 @@
 #
 # Neural Network
 #
-import numpy as np
 import random as random
+import numpy as np
+
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=invalid-name
+
 
 class NeuralNetwork:
     '''NeuralNetwork'''
-    def __init__(self, input_nodes, hidden_nodes, output_nodes):
-        self.input_nodes = input_nodes
-        self.hidden_nodes = hidden_nodes
-        self.output_nodes = output_nodes
-        self.weights_ih = np.matrix(np.random.random((self.hidden_nodes, self.input_nodes))) / self.input_nodes
-        self.weights_ho = np.matrix(np.random.random((self.output_nodes, self.hidden_nodes))) / self.hidden_nodes
-        self.bias_h = np.matrix(np.random.random((self.hidden_nodes, 1))) / self.input_nodes
-        self.bias_o = np.matrix(np.random.random((self.output_nodes, 1))) / self.hidden_nodes
-        self.learning_rate = 0.1
+
+    def __init__(self, input_nodes=0, hidden_nodes=0, output_nodes=0, parent_a=None, parent_b=None, mutation_rate=0):
+        if(parent_a is not None) and (parent_b is not None):
+            self.learning_rate = parent_a.learning_rate
+            self.input_nodes = parent_a.input_nodes
+            self.hidden_nodes = parent_a.hidden_nodes
+            self.output_nodes = parent_a.output_nodes
+            self._mix_and_mutate(parent_a, parent_b, mutation_rate)
+        else:
+            self.learning_rate = 0.1
+            self.input_nodes = input_nodes
+            self.hidden_nodes = hidden_nodes
+            self.output_nodes = output_nodes
+            self.weights_ih = np.matrix(np.random.random(
+                (self.hidden_nodes, self.input_nodes))) / self.input_nodes
+            self.weights_ho = np.matrix(np.random.random(
+                (self.output_nodes, self.hidden_nodes))) / self.hidden_nodes
+            self.bias_h = np.matrix(np.random.random(
+                (self.hidden_nodes, 1))) / self.input_nodes
+            self.bias_o = np.matrix(np.random.random(
+                (self.output_nodes, 1))) / self.hidden_nodes
 
     def __str__(self):
         return 'NeuralNetwork[%s, %s, %s] :\n  Hidden : \n%s\n  Ouput : \n%s' % (self.input_nodes, self.hidden_nodes, self.output_nodes, self.weights_ih, self.weights_ho)
-    
+
+    def clone(self):
+        new_neural_network = NeuralNetwork(
+            self.input_nodes, self.hidden_nodes, self.output_nodes)
+        new_neural_network.weights_ih = self.weights_ih.copy()
+        new_neural_network.weights_ho = self.weights_ho.copy()
+        new_neural_network.bias_h = self.bias_h.copy()
+        new_neural_network.bias_o = self.bias_o.copy()
+        new_neural_network.learning_rate = self.learning_rate
+        return new_neural_network
+
+    def get_dna(self):
+        to_dna = list()
+        to_dna.append(np.array(self.weights_ih).flatten())
+        to_dna.append(np.array(self.weights_ho).flatten())
+        to_dna.append(np.array(self.bias_h).flatten())
+        to_dna.append(np.array(self.bias_o).flatten())
+        return np.concatenate(to_dna)
+
+    def set_dna(self, dna):
+        self.weights_ih = np.matrix(
+            dna[:(self.input_nodes*self.hidden_nodes)]).reshape(self.hidden_nodes, self.input_nodes)
+        dna = dna[(self.input_nodes*self.hidden_nodes):]
+        self.weights_ho = np.matrix(
+            dna[:(self.hidden_nodes*self.output_nodes)]).reshape(self.output_nodes, self.hidden_nodes)
+        dna = dna[(self.hidden_nodes*self.output_nodes):]
+        self.bias_h = np.matrix(dna[:self.hidden_nodes]).reshape(
+            self.hidden_nodes, 1)
+        dna = dna[self.hidden_nodes:]
+        self.bias_o = np.matrix(dna[:self.output_nodes]).reshape(
+            self.output_nodes, 1)
+        dna = dna[self.output_nodes:]
+
+    def _mix_and_mutate(self, parent_a, parent_b, mutation_rate):
+        dna_a = parent_a.get_dna()
+        dna_b = parent_b.get_dna()
+        split = random.randint(0, len(dna_a))
+        dna = np.concatenate((dna_a[0:split], dna_b[split:len(dna_a)]))
+
+        for x in np.nditer(dna, op_flags=['readwrite']):
+            if random.random() < mutation_rate:
+                x[...] = random.random()
+
+        self.set_dna(dna)
+
+    # def mutate(self, rate):
+    #     for x in np.nditer(self.weights_ih, op_flags=['readwrite']):
+    #         if random.random() < rate:
+    #             x[...] = random.random()
+    #     for x in np.nditer(self.weights_ho, op_flags=['readwrite']):
+    #         if random.random() < rate:
+    #             x[...] = random.random()
+    #     for x in np.nditer(self.bias_h, op_flags=['readwrite']):
+    #         if random.random() < rate:
+    #             x[...] = random.random()
+    #     for x in np.nditer(self.bias_o, op_flags=['readwrite']):
+    #         if random.random() < rate:
+    #             x[...] = random.random()
+
+    def set_to(self, value=0):
+        for x in np.nditer(self.weights_ih, op_flags=['readwrite']):
+            x[...] = value
+        for x in np.nditer(self.weights_ho, op_flags=['readwrite']):
+            x[...] = value
+        for x in np.nditer(self.bias_h, op_flags=['readwrite']):
+            x[...] = value
+        for x in np.nditer(self.bias_o, op_flags=['readwrite']):
+            x[...] = value
+
     def predict(self, input_data):
         ''' Feed forward function '''
         inputs = np.matrix(input_data).transpose()
@@ -30,7 +114,8 @@ class NeuralNetwork:
         # hidden = np.add(hidden, self.bias_h)
         hidden = hidden + self.bias_h
         # hidden = np.array([self.sigmoid(x) for x in hidden])
-        hidden = np.matrix(np.array([self.sigmoid(x) for x in hidden])).transpose()
+        hidden = np.matrix(np.array([self.sigmoid(x)
+                                     for x in hidden])).transpose()
 
         # Generating outputs layer values
         # outputs = np.dot(self.weights_ho, hidden)
@@ -38,7 +123,8 @@ class NeuralNetwork:
         # outputs = np.add(outputs, self.bias_o)
         outputs = outputs + self.bias_o
         # outputs = np.array([self.sigmoid(x) for x in outputs])
-        outputs = np.matrix(np.array([self.sigmoid(x) for x in outputs])).transpose()
+        outputs = np.matrix(np.array([self.sigmoid(x)
+                                      for x in outputs])).transpose()
 
         return outputs.flatten()
 
@@ -68,7 +154,8 @@ class NeuralNetwork:
         # hidden = np.add(hidden, self.bias_h)
         hidden = hidden + self.bias_h
         # hidden = np.array([self.sigmoid(x) for x in hidden])
-        hidden = np.matrix(np.array([self.sigmoid(x) for x in hidden])).transpose()
+        hidden = np.matrix(np.array([self.sigmoid(x)
+                                     for x in hidden])).transpose()
 
         # Generating outputs layer values
         # outputs = np.dot(self.weights_ho, hidden)
@@ -76,7 +163,8 @@ class NeuralNetwork:
         # outputs = np.add(outputs, self.bias_o)
         outputs = outputs + self.bias_o
         # outputs = np.array([self.sigmoid(x) for x in outputs])
-        outputs = np.matrix(np.array([self.sigmoid(x) for x in outputs])).transpose()
+        outputs = np.matrix(np.array([self.sigmoid(x)
+                                      for x in outputs])).transpose()
 
         #
         # Back propagation
@@ -85,8 +173,10 @@ class NeuralNetwork:
         # Output layer error
         errors_o = targets - outputs
         # Calculate output gradient
-        output_gradients = np.matrix(np.array([self.desigmoid(x) for x in outputs])).transpose()
-        output_gradients = np.multiply(output_gradients, errors_o) * self.learning_rate
+        output_gradients = np.matrix(
+            np.array([self.desigmoid(x) for x in outputs])).transpose()
+        output_gradients = np.multiply(
+            output_gradients, errors_o) * self.learning_rate
         # Adjust output bias
         self.bias_o += output_gradients
         # Calculate deltas
@@ -97,8 +187,10 @@ class NeuralNetwork:
         # Hidden layer error
         errors_h = np.dot(self.weights_ho.transpose(), errors_o)
         # Calculate hidden gradient
-        hidden_gradients = np.matrix(np.array([self.desigmoid(x) for x in hidden])).transpose()
-        hidden_gradients = np.multiply(hidden_gradients, errors_h) * self.learning_rate
+        hidden_gradients = np.matrix(
+            np.array([self.desigmoid(x) for x in hidden])).transpose()
+        hidden_gradients = np.multiply(
+            hidden_gradients, errors_h) * self.learning_rate
         # Adjust hidden bias
         self.bias_h += hidden_gradients
         # Calculate deltas
@@ -115,36 +207,6 @@ class NeuralNetwork:
         # return sigmoid(x) * (1 - sigmoid(x))
         return y * (1 - y)
 
-def testNN(nn):
-    print(nn.predict([1, 0]))
-    print(nn.predict([0, 1]))
-    print(nn.predict([1, 1]))
-    print(nn.predict([0, 0]))
-
-def trainNN(nn, iterations):
-    data = [
-        {
-            'inputs': [1, 0],
-            'target': [1]
-        },
-        {
-            'inputs': [0, 1],
-            'target': [1]
-        },
-        {
-            'inputs': [1, 1],
-            'target': [0]
-        },
-        {
-            'inputs': [0, 0],
-            'target': [0]
-        },
-    ]
-
-    for _ in range(iterations):
-        x = random.choice(data)
-        nn.train(x['inputs'], x['target'])
-
 
 #
 # Main
@@ -154,16 +216,30 @@ if __name__ == '__main__':
     print("***** Main...")
     print("*****")
 
-    # Basic tests
-    # nn = NeuralNetwork(2, 2, 2)
-    # inputs = np.array([1, 0])
-    # targets = np.array([1, 1])
-    # nn.train(inputs, targets)
+    NN_A = NeuralNetwork(3, 5, 2)
+    NN_A.set_to(0)
+    # print(NN_A)
+    # NN_B = NN_A.clone()
+    # NN_A.mutate(0.5)
+    # print(NN_B)
+    # print(NN_A)
 
-    # Neural network training for XOR
-    nn = NeuralNetwork(2, 3, 1)
-    print('***** Before training')
-    testNN(nn)
-    trainNN(nn, 100000)
-    print('***** After training')
-    testNN(nn)
+    NN_B = NeuralNetwork(3, 5, 2)
+    NN_B.set_to(1)
+    # NN_B.set_dna(NN_A.get_dna())
+
+    # dna_a = NN_A.get_dna()
+    # dna_b = NN_B.get_dna()
+    # split = random.randint(0, len(dna_a))
+    # dna = np.concatenate((dna_a[0:split], dna_b[split:len(dna_a)]))
+
+    # print(dna_a)
+    # print('----------')
+    # print(dna_b)
+    # print('----------')
+    # print(dna)
+
+    # print(NN_A)
+    # print('----------')
+    NN_C = NeuralNetwork(parent_a=NN_A, parent_b=NN_B, mutation_rate=0.01)
+    print(NN_C)
